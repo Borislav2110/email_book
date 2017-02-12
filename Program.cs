d194655fe15dc4e3a9affc723af47722495876d2
Borislav Popov <borko200010@gmail.com>
2017-02-12 10:45:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApplication11
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var emailbook = new Dictionary<string, string>();
+            var input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                var name = input;
+                var email = Console.ReadLine().Split('.').ToList();
+                if (email[1] != "us" || email[1] != "uk")
+                {
+                    emailbook[name] += email;
+                }
+                input = Console.ReadLine();
+            }
+            foreach(var name in emailbook)
+            {
+                var Name = name.Key;
+                var Number = name.Value;
+                Console.WriteLine($"{Name} -> {Number} ");
+            }
+
+        }
+    }
+}
